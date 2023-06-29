ec752065d9d012773dbb30a42a292ee4dfae027d
Edward <crazybber@outlook.com>
2023-06-29 18:11:12 +0800

    add date format

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,21 @@
 import Layout from "../../components/layout";
 import { getAllPostIds,getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+
 
 export default function Post({postData}) {
     return(
         <Layout> 
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+
         {postData.title}
         <br />
         {postData.id}
+        <br />
+        <Date dateString={postData.date} />
         <br />
         {postData.date}
         <br />
